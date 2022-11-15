596f659999959e239d8872271cbbcb2b136fa777
MasterTDF <117050184+MasterTDF@users.noreply.github.com>
2022-11-15 10:03:56 +0100

    Importing items in game

---
+++ src/main/java/com/mastertdf/jobs/items/JobsItemGroup.java
@@ -0,0 +1,20 @@
+package com.mastertdf.jobs.items;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class JobsItemGroup {
+    public static final ItemGroup JOBS_GROUP = new ItemGroup("JobsModTab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(JobsItems.Jobs_Book.get());
+            return new ItemStack(JobsItems.Jobs_Book_Artisan.get());
+            return new ItemStack(JobsItems.Jobs_Book_Engineer.get());
+            return new ItemStack(JobsItems.Jobs_Book_Farmer.get());
+            return new ItemStack(JobsItems.Jobs_Book_Hunter.get());
+            return new ItemStack(JobsItems.Jobs_Book_Smith.get());
+            return new ItemStack(JobsItems.Jobs_Book_Wizard.get());
+            return new ItemStack(JobsItems.Shard_of_xp.get());
+        }
+    }
+}
